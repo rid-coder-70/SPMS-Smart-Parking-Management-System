cd3feb00951aa23f1cedc8850f6ae5440c4b81b2
Marfi-76 <mahdiulhassanmarfi2022@gmail.com>
2026-06-22 22:51:58 +0600

    feat: add loader component and animations for enhanced UI experience

---
+++ frontend/src/components/ui/loader-9.tsx
@@ -0,0 +1,35 @@
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+
+export const Component = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div
+      className={cn("loader")}
+      data-count={count}
+      onClick={() => setCount((current) => current + 1)}
+    >
+      <div className="box box-1">
+        <div className="side-left"></div>
+        <div className="side-right"></div>
+        <div className="side-top"></div>
+      </div>
+      <div className="box box-2">
+        <div className="side-left"></div>
+        <div className="side-right"></div>
+        <div className="side-top"></div>
+      </div>
+      <div className="box box-3">
+        <div className="side-left"></div>
+        <div className="side-right"></div>
+        <div className="side-top"></div>
+      </div>
+      <div className="box box-4">
+        <div className="side-left"></div>
+        <div className="side-right"></div>
+        <div className="side-top"></div>
+      </div>
+    </div>
+  );
+};
